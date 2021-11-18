d3231b2c4962116d62f04ca42bf2ca5646a68472
Sujan Neupane <85225585+SujanNeupane42@users.noreply.github.com>
2021-11-18 20:09:51 +0545

    update app,py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 st.set_page_config(layout="wide")
 df = pd.read_csv('winequality-red.csv')
